6987a17b8c68c9144608db6a140ebdf96043e793
Antoine <antoinejuillard015@gmail.com>
2023-04-12 06:32:55 -0500

    creation titre

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,20 +6,33 @@ function injectToDom() {
     const Article = document.querySelector("main section:nth-child(2)");
     const sectionArticle = document.createElement("article");
     const titre = document.createElement("h2");
-    const titre2 = document.createElement("h2")
+    const titre2 = document.createElement("h3");
+    const titre3 = document.createElement("h3");
+
+    const para = document.createElement("p");
+
+    para.textContent = datas.section.multiFish.para.p[0]+" "+ datas.section.multiFish.para[1];
+
+
     titre.textContent = datas.section.title[0]+' '+datas.section.title[1];
-    titre2.textContent = datas.multiFish.title;
+    titre3.textContent = datas.section.crustyFish.title;
+    titre2.textContent = datas.section.multiFish.title;
+
+
+    titre2.style.margin = "30px";
+    titre3.style.margin = "30px";
 
     console.log(titre2.textContent)
 
     sectionArticle.style.width = "100%";
     Article.append(sectionArticle)
-    sectionArticle.append(titre)
 
-    sectionArticle.appendChild(titre2)
+   
+    sectionArticle.append(titre);
+    sectionArticle.append(titre3);
+    sectionArticle.append(para);
+    sectionArticle.append(titre2);
 
-
-    console.log(Article)
 
 
 }
